34aaf06dc79673ec8f2dd9a2f2b1871040767f00
Valorando <vadimsokil.dnipro@gmail.com>
2023-11-09 11:50:56 +0200

    Realization

--- Homework_09_11_II/Program.cs
+++ Homework_09_11_II/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Homework_09_11_II
 {
     internal class Program
     {
+        static int[] numbers = new int[10] { 5, 8, 12, 6, 74, 56, 7, 21, 9, 15 };
+        static Mutex mutex = new Mutex();
+
+        static void Original_array(int[] arr)
+        {
+            mutex.WaitOne();
+            Console.WriteLine("Оригинальный массив: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Thread.Sleep(1000);
+            mutex.ReleaseMutex();
+        }
+        
+        static void Modified_array(int[] arr)
+        {
+            mutex.WaitOne();
+            Console.WriteLine("Массив, элементы которого увеличены на 5:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] += 5);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Thread.Sleep(1000);
+            mutex.ReleaseMutex();
+        }
+
+        static void Max_element(int[] arr)
+        {
+            mutex.WaitOne();
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine($"Максимальный элемент: {arr.Max()}");
+            }
+            mutex.ReleaseMutex();
+        }
+
         static void Main(string[] args)
         {
+            Thread t1 = new Thread(() => Original_array(numbers));
+            Thread t2 = new Thread(() => Modified_array(numbers));
+            Thread t3 = new Thread(() => Max_element(numbers));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Console.Read();
         }
     }
 }
--- Homework_09_11_III/Form1.Designer.cs
+++ Homework_09_11_III/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(128, 290);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(146, 36);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(128, 290);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Thread1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Thread2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(287, 373);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(303, 412);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(303, 412);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
